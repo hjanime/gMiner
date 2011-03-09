5a5674ba84c99b467ef25a145f43ac98bc3d7ce9
Lucas Sinclair <lucas.sinclair@me.com>
2011-03-09 16:56:50 +0100

    More descriptive error message

--- Clients/client.java
+++ Clients/client.java
@@ -8,72 +8,72 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Client {    
-	// Build parameter string for gMiner
-	public static final String content = "[gMiner]" + "\n" + "version=0.1.5"
+    // Build parameter string for gMiner
+    public static final String content = "[gMiner]" + "\n" + "version=0.1.5"
+    + "\n" + "track1=/scratch/tracks/qual/sql/ribosome_genesis.sql"
+    + "\n" + "track1_name='Fake client track one'"
+    + "\n" + "track2=/scratch/tracks/qual/sql/ribosome_proteins.sql"
+    + "\n" + "track2_name='Fake client track two'"
+    + "\n" + "operation_type=desc_stat"
+    + "\n" + "characteristic=number_of_features"
+    + "\n" + "per_chromosome=True"
+    + "\n" + "compare_parents=False"
+    + "";
 
-	+ "\n" + "track1=/scratch/tracks/qual/sql/ribosome_genesis.sql"
-	+ "\n" + "track1_name='Fake client track one'"
-	+ "\n" + "track2=/scratch/tracks/qual/sql/ribosome_proteins.sql"
-	+ "\n" + "track2_name='Fake client track two'"
-	+ "\n" + "operation_type=desc_stat"
-	+ "\n" + "characteristic=number_of_features"
-	+ "\n" + "per_chromosome=True"
-	+ "\n" + "compare_parents=False"
-	+ "";
+    // The URL for posting
+    public static final String address = "http://www.example.com/gMiner/";
 
-	// The url for posting
-	public static final String address = "http://sugar.epfl.ch/gMiner/";
+    public void sendPostRequest() {
+        try {
+            // URL of web service
+            URL url = new URL(address);
+            // HTTP connection channel
+            HttpURLConnection   conn =  (HttpURLConnection) url.openConnection();
+            // Let the run-time system (RTS) know that we want input.
+            conn.setDoInput(true);
+            // Let the RTS know that we want to do output
+            conn.setDoOutput(true);
+            // Specify the method
+            conn.setRequestMethod("POST");
+            // No caching, we want the real thing.
+            conn.setUseCaches(false);
+            // Specify the content type and size.
+            conn.setRequestProperty("Content-Type", "text/plain");
+            conn.setRequestProperty("Content-Length", Integer.toString(content.length()));
+            // Create output stream
+            DataOutputStream output = new DataOutputStream(conn.getOutputStream());
+            // Send POST output.
+            output.writeBytes(content);
+            output.flush();
+            output.close();
+            // Output the response code
+            System.out.println(conn.getResponseCode());
+            // Get the input stream
+            DataInputStream inStream;
+            try {
+                inStream  = new DataInputStream(conn.getInputStream());
+            } catch (IOException e) {
+                inStream = new DataInputStream(conn.getErrorStream());
+            }
+            // Get the result as a string
+            String buffer,result = "";
+            InputStreamReader isr = new InputStreamReader(inStream);
+            BufferedReader br = new BufferedReader(isr);
+            while(null != (buffer = br.readLine())){
+                result+=buffer+"\n";
+            }
+            // Close the stream
+            inStream.close();
+            // Output the result
+            System.out.println(result);
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void sendPostRequest() {
-		try {            
-			// URL of web service
-			URL url = new URL(address);
-			// HTTP connection channel.
-			HttpURLConnection   conn =  (HttpURLConnection) url.openConnection();
-			// Let the run-time system (RTS) know that we want input.
-			conn.setDoInput(true);
-			// Let the RTS know that we want to do output.
-			conn.setDoOutput(true);
-			// Specify the method
-			conn.setRequestMethod("POST");
-			// No caching, we want the real thing.
-			conn.setUseCaches(false);
-			// Specify the content type and size.
-			conn.setRequestProperty("Content-Type", "text/plain");
-			conn.setRequestProperty("Content-Length",Integer.toString(content.length()));
-			// Create output stream
-			DataOutputStream output = new DataOutputStream(conn.getOutputStream());
-			// Send POST output.
-			output.writeBytes(content);
-			output.flush();
-			output.close();
-			//output the response code
-			System.out.println(conn.getResponseCode());
-			//get the input stream
-			DataInputStream inStream;
-			try {
-				inStream  = new DataInputStream(conn.getInputStream());
-			} catch (IOException e) {
-				inStream = new DataInputStream(conn.getErrorStream());
-			} 
-			//get the result as a String
-			String buffer,result = "";
-			InputStreamReader isr = new InputStreamReader(inStream);
-			BufferedReader br = new BufferedReader(isr);
-			while(null!=(buffer = br.readLine())){
-				result+=buffer+"\n";
-			}
-			// Close the stream
-			inStream.close();
-			//output the result
-			System.out.println(result);
-		} catch (MalformedURLException ex) {
-			ex.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public static void main(String[] args) {
-		new Client().sendPostRequest();
-	}
+    public static void main(String[] args) {
+        new Client().sendPostRequest();
+    }
 }
