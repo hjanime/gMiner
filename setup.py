1f11e1f6517a124c6d4813ba3a3d2e3940d5131d
Lucas Sinclair <lucas.sinclair@me.com>
2011-05-16 12:47:28 +0200

    Incrementing version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name             = 'gMiner',
-    version          = '1.2.0',
+    version          = '1.3.0',
     packages         = ['gMiner', 
                         'gMiner.operations',
                         'gMiner.operations.desc_stat',
